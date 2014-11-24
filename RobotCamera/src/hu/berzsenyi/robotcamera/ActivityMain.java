ce6d8da135617edc60f91b1f14c506d3786926b3
Balázs Fehér <fbalazs369@gmail.com>
2014-11-24 17:50:42 +0100

    Android project

---
+++ RobotCamera/src/hu/berzsenyi/robotcamera/ActivityMain.java
@@ -0,0 +1,12 @@
+package hu.berzsenyi.robotcamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_main);
+	}
+}
